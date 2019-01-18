56e31dd4354f13446bce01d7fe373764ef0814d7
lzcwds <www.duorz.com>
2019-01-18 11:13:21 +0800

    init

---
+++ src/main/main.js
@@ -0,0 +1,44 @@
+import { app, BrowserWindow } from 'electron';
+import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer';
+import { enableLiveReload } from 'electron-compile';
+
+let mainWindow;
+
+const isDevMode = process.execPath.match(/[\\/]electron/);
+
+if (isDevMode) enableLiveReload();
+
+const createWindow = async () => {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  mainWindow.loadURL(`file://${__dirname}/../render/index.html`);
+
+  if (isDevMode) {
+    await installExtension(VUEJS_DEVTOOLS);
+    mainWindow.webContents.openDevTools();
+  }
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+};
+
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
---
+++ src/render/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <button @click="test()">新建截图</button>
+        <h2>Hello from {{text}}</h2>
+    </div>
+</template>
+
+<script>
+    import {remote} from 'electron'
+    const {BrowserWindow} = remote
+	export default {
+		name: 'index',
+		mounted() {
+            console.log('mounted')
+		},
+		data() {
+			return {
+				text: '',
+                win:null
+			}
+		},
+		methods:{
+			test(){
+				this.win = new BrowserWindow({
+					width: 800,
+					height: 600,
+                })
+				this.win.loadURL(`file://${__dirname}/shortcut/shortcut.html`);
+            }
+        }
+	}
+</script>
